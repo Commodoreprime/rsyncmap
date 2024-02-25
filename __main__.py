c6e5640bac7cec47b09c34741b96fcdbfd27793d
Commodoreprime <35617951+Commodoreprime@users.noreply.github.com>
2024-02-25 11:43:43 -0900

    Add trailing slash detection

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import pathlib as pl
 from sys import argv
 from pprint import pprint
+import os
 
 from rsync_helper import *
 from utils import *
@@ -30,9 +31,9 @@ with open(root_syncmap) as f:
         if argus[1] == "=>":
             syncmap_abstract.update({
                 i: {
-                    "from": argus[0],
-                    "to": argus[2],
-                    "filter_rules": []
+                    "from": (argus[0], argus[0].endswith(os.sep)),
+                    "to": (argus[2], argus[2].endswith(os.sep)),
+                    "filter_rules": [],
                 }
             })
             last_idx = i
@@ -40,8 +41,8 @@ with open(root_syncmap) as f:
         if last_idx == None:
             syncmap_abstract.update({
                 0: {
-                    "from": "",
-                    "to": "",
+                    "from": ("", True),
+                    "to": ("", False),
                     "filter_rules": []
                 }
             })
@@ -56,6 +57,12 @@ for i in syncmap_abstract.keys():
     if len(entry["filter_rules"]) > 0:
         for filter in entry["filter_rules"]:
             filter_args.append(f"--filter={filter}")
-    rsync(str(source_directory.joinpath(entry["from"])), str(target_directory.joinpath(entry["to"])), [additional_args, filter_args], dry_run)
+    final_source_path = str(source_directory.joinpath(entry["from"][0]))
+    final_dest_path = str(target_directory.joinpath(entry["to"][0]))
+    if entry["from"][1] == True:
+        final_source_path = f"{final_source_path}{os.sep}"
+    if entry["to"][1] == True:
+        final_dest_path = f"{final_dest_path}{os.sep}"
+    rsync(final_source_path, final_dest_path, [additional_args, filter_args], dry_run)
 
 exit(0)
