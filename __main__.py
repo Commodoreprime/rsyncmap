491d7d1e6b90835a790e98edeb8dc24f3d027fd6
Commodoreprime <35617951+Commodoreprime@users.noreply.github.com>
2024-02-24 23:14:16 -0900

    Committing any extra changes

--- __main__.py
+++ __main__.py
@@ -9,36 +9,30 @@ import syncmap
 def pprint(obj):
     print(jdumps(obj, indent=2, default=str))
 
-syncmaps:dict = {}
-for syncmap_file in sorted(syncmap.get_file_list(args.initial_directory), reverse=True):
-    print(syncmap_file)
-    syncmaps[os.path.dirname(syncmap_file)] = syncmap.parse_file(syncmap_file)
-
-pprint(syncmaps)
-
-for map_key in syncmaps:
-    operation_list = syncmaps[map_key]
+for syncmap_file in syncmap.get_file_list(args.initial_directory):
+    parent_directory = os.path.dirname(syncmap_file)
+    operation_list = syncmap.parse_file(syncmap_file)
     negation_list:list = []
-    for map_item in operation_list:
-        operation:syncmap.GenericOperator = map_item["operation"]
-        if type(operation) == syncmap.NegationOperator:
-            negation_list.append(operation.exclude)
+    arguments_stack:list = []
+    arguments_stack.append("--mkpath")
+    for i, operation in enumerate(operation_list):
+        filter_rules:list = []
+        if i == 0: arguments_stack.clear()
+        if type(operation) == syncmap.ArgumentsOperator:
+            veprint(f"Attemping to modify the arguments stack with these values (removing? {operation.subtractive}): {operation.arguments}")
+            if operation.subtractive == False:
+                arguments_stack.extend(operation.arguments)
+            else:
+                for arg in operation.arguments:
+                    if arguments_stack.count(arg) > 0:
+                        arguments_stack.remove(arg)
         elif type(operation) == syncmap.DirectionOperator:
-            source_path:str = map_key
-            additonal_filters = []
+            source_path:str = parent_directory
             if operation.is_dir == True:
-                source_path = os.path.join(source_path, operation.from_target)
-                for negation in negation_list:
-                    additonal_filters.append(negation.replace(operation.from_target, ''))
-            elif operation.is_globbed == False:
-                source_path = os.path.join(source_path, operation.from_target)
-            if operation.is_globbed == True:
-                dir_glob_pair:list = operation.from_target.rsplit('/', maxsplit=1)
-                source_path = os.path.join(source_path, dir_glob_pair[0]+'/')
-                additonal_filters.append("-m")
-                additonal_filters.append("-f+ */")
-                additonal_filters.append(f"-f+ {dir_glob_pair[1]}")
-                additonal_filters.append("-f- *")
-            rsync(source_path, operation.to_path, [ args.default_flags,
-                                                    map_item["opt_args"],
-                                                    additonal_filters ], args.dry_run)
+                if (arguments_stack.count("--recursive") + arguments_stack.count("-r")) == 0:
+                    arguments_stack.append("--recursive")
+                source_path = source_path+'/'
+            filter_rules.append(f"--include={operation.from_path}")
+            filter_rules.append("--exclude=*")
+            rsync(source_path, os.path.join(args.destination_directory, operation.to_path), [args.default_flags, filter_rules, arguments_stack], args.dry_run)
+    break
